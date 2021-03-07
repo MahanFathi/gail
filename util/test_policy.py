842eebe0ef89816185f39db24527384c42458cbd
MahanFathi <mahan0fathi@gmail.com>
2021-03-07 13:34:36 -0500

    Fix normalization when testing out trained policy

--- util/test_policy.py
+++ util/test_policy.py
@@ -1,14 +1,13 @@
 import os
 import gym
 import stable_baselines3 as sb3
-from stable_baselines3.common.vec_env import VecNormalize
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 
 from imitation.util import util, logger
 
-def test_policy(policy: sb3.PPO, iteration: int, model_path=None, env_stats_path=None, normalize=True):
+def test_policy(policy: sb3.PPO, env:gym.Env, iteration: int, model_path=None, env_stats_path=None, normalize=True):
 
-
-    env = policy.get_env()
+    env = DummyVecEnv([lambda: env.unwrapped])
     if normalize:
         env_stats_path = env_stats_path or os.path.join("./envs", "{}.pkl".format(iteration))
         env = VecNormalize.load(env_stats_path, env)
@@ -16,7 +15,7 @@ def test_policy(policy: sb3.PPO, iteration: int, model_path=None, env_stats_path
     env.norm_reward = False
 
     model_path = model_path or os.path.join("./models", "{}.zip".format(iteration))
-    policy.load(model_path, env=env)
+    policy = policy.load(model_path, env=env)
 
     obs = env.reset()
     for _ in range(1000):
